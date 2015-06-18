521047f5bec425027c348ead6c06a450df4b0458
Kevin Martensson <kevinmartensson@gmail.com>
2015-06-18 18:10:23 +0200

    Add input test

--- test.js
+++ test.js
@@ -8,3 +8,15 @@ test(function (t) {
 	t.assert(!isNegative(1));
 	t.end();
 });
+
+test('throws if input isn\'t a number', function (t) {
+	t.plan(2);
+
+	try {
+		isNegative('foo');
+		t.fail();
+	} catch (err) {
+		t.assert(err);
+		t.assert(err.message === 'Expected a number');
+	}
+});
